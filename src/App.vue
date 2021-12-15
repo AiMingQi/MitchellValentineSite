5b201d9adfff5cd54b3d6a977d050d3ba7e489b5
AiMingQi <cafevalentine@gmail.com>
2021-12-15 15:13:45 -0700

    site files

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#c00000"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <h2 class="ml-2"> Mitchell Valentine</h2>
+        
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/aimingqi"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">My Github</span>
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,186 @@
+<template lang="pug">
+  v-container(fluid)
+    v-row(dense)
+      v-col(cols="12" md="4")
+        v-card.pa-3.ma-3
+          v-card-text
+            v-img(src="../assets/logo.png" alt="logo" max-width="800px")
+          v-card-actions
+            div.text-center
+              v-btn.ma-3(href="mailto:cafevalentine@gmail.com" target="_blank" rel="noopener")
+                v-icon(large) mdi-email
+              v-btn.ma-3(href="https://www.linkedin.com/in/cafevalentine/" target="_blank" rel="noopener")
+                v-icon(large) mdi-linkedin
+              v-btn.ma-3(href="https://facebook.com/mitchellvalentine" target="_blank" rel="noopener")
+                v-icon(large) mdi-facebook
+              v-btn.ma-3(href="https://www.youtube.com/c/MitchellValentine" target="_blank" rel="noopener")
+                v-icon(large) mdi-youtube
+              v-btn.ma-3(href="https://twitter.com/CafeValentine" target="_blank" rel="noopener")
+                v-icon(large) mdi-twitter
+              v-btn.ma-3(href="https://www.instagram.com/aimingqi/" target="_blank" rel="noopener")
+                v-icon(large) mdi-instagram
+              v-btn.ma-3(href="https://github.com/aimingqi" target="_blank" rel="noopener")
+                v-icon(large) mdi-github
+              v-btn.ma-3(href="https://unitegrity.com" target="_blank" rel="noopener")
+                v-icon(large) mdi-web
+              v-btn.ma-3(href="https://the-game.fun" target="_blank" rel="noopener")
+                v-icon(large) mdi-web
+      v-col(cols="12" md="8")
+        v-card.pa-3.ma-3
+          v-card-title 
+            h1 Welcome to Mitchell Valentine's
+          v-card-subtitle 
+            h2 History and Experience Exploratorium
+          v-card-text.pa-3
+            h3 Resides in: Centennial, CO
+            h3 Born in: Englewood, CO
+            h3 Birthday: March 24, 1979
+            br
+            h3 Family: Crystal Valentine, Ani Valentine, &amp; Scarlett Valentine
+            br
+            h3 High School: Rye High School 
+            h3 College: University of Colorado at Boulder 
+            h3 Degree: Environmental, Organismic, and Population Biology
+    v-row
+      v-col
+        v-card.d-flex(dark)
+          v-card-text
+            h1 Major Projects:
+            br
+            h2 Web Sites:
+            br
+            h3 Active:
+            br
+            ul
+              li 
+                a(href="https://the-game.fun" target="_blank" ) the-game.fun
+              li 
+                a(href="https://Unitegrity.com" target="_blank" ) Unitegrity.com
+              li 
+                a(href="https://ComradeBrewing.com" target="_blank" ) ComradeBrewing.com
+              li 
+                a(href="https://CorrectiveBodySolutions.com" target="_blank" ) CorrectiveBodySolutions.com
+              li 
+                a(href="https://Swampthing.org" target="_blank" ) Swampthing.org
+            br
+            h3 In-Active:
+            br
+            ul  
+              li CFFP.edu (Became KaplanFinancial.com)
+              li ParamountBooking.com (no longer active)
+              li Drone-Pilot-Zone.com (no longer active)
+              li Those-Funken-Dronez.com (no longer active)
+              li Crypto-Catalyst.com (no longer active)
+            br
+            h2 Live Experiences:
+            br
+            ul
+              li Xiamen Beach Parties
+              li Those Funken Dronez
+              li Route 66
+    v-row
+      v-col
+        v-card.d-flex(dark)
+          v-card-text
+            h1 Skills: 
+            br
+            h2 Human Languages:
+            br
+            ul
+              li English
+              li Spanish
+              li Mandarin Chinese
+            br
+            h2 Computer Languages:
+            br
+            ul
+              li Html / CSS / JavaScript
+              li Rust
+              li Solidity / Vyper
+              li Haskell / Plutus
+              li VueJS
+            br
+            h2 Hobbies:
+            br
+            ul 
+              li Cryptocurrency Development
+              li 3D Printing
+              li FPV Drone Racing
+              li Aquaponics &amp; Exotic Pets 
+            br
+            h2 Hackathons, Clubs &amp; Organizations:
+            br
+            ul
+              li 
+                a(href="https://multigp.com" target="_blank" ) MultiGP FPV Drone Racing Association
+              li 
+                a(href="https://ethdenver.com" target="_blank" ) ETHDenver
+            br
+    v-row
+      v-col
+        v-card(dark)
+          v-card-title 
+            h1 Career: 
+          v-card-text
+            h2 Kaplan North America | 2014 - Present
+            p In 2018 KNA acquired the College for Financial Planning and I was promoted to develop the company’s presence on Amazon. I was then advanced into a Pricing Analyst role. My role is strategy, process development, product selection, pricing, advertising, reporting, and corporate communications.
+            h3 - College for Financial Planning | 2014- 2018
+            p I made the College look amazing. Planning, design, development, graphic design, layout, implementation and management of CFFP.edu. All design of public facing materials including print, web, certificates, logos, corprate presentations, and mailings.
+            br 
+            h2 The UniTEGRITY Group LLC | 2018 - Present
+            strong Winner of InterPlanetary DAO at ETHDenver2020
+            p A group focused on developing web3 projects. Projects utilizing Non Fungible Tokens (NFTs) to create enhanced consumer transparency, privacy, equality and trust. 
+            p Main Tech: Ethereum, Cardano, Solana, Solidity, Plutus, Vyper, Rust, IPFS decentralized networking stacks, DeFi, and DAOs, VueJS 
+            br 
+            h2 An Long Productions | 2010-2014
+            strong Built 100s of projects affecting millions of people
+            p Curriculum development and implementation, web development, logo design, video production, Swedish TV reality star, event production, talent acquistion, marketing, consulting, eCommerce, branding services, project planning, management, and grunt work.
+            br 
+            h2 Classic Cafe | 2012-2014
+            h3 Manager / Server
+            br 
+            h2 Cornerstone Prep | 2011
+            h3 Curriculum Developer
+            br 
+            h2 Xiamen Yingcai School | 2007-2010
+            h3 Academic Director
+            br 
+            h2 BookEnd Cafe | 2001-2006
+            h3 General Manager
+            br 
+            h2 Foley's Department Store | 1999 - 2001
+            h3 Men's Department and Textiles Inventory Manager
+            br 
+            h2 University of Colorado Dining Hall | 1997 - 1999
+            h3 Cook's Assistant
+            br 
+            h2 HollyDot Golf Course | 1998
+            h3 Greens Keeper
+            br 
+            h2 Lamplight Cafe | 1997
+            h3 Barista
+            br 
+            h2 Midwest Steel | 1997
+            h3 Welder
+            br 
+            h2 Pizza Stop | 1995-1997
+            h3 Delivery Driver
+            br 
+            h2 Tradewinds Sailing Club | 1989-1993
+            h3 Boat Maintenance and Cleaning
+
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
